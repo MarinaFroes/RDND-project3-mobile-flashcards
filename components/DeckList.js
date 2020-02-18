3e6585e751f8b9526702f3724c45dc0ba040d7e7
MFAC <ninafroes@gmail.com>
2020-02-18 23:14:33 +0100

    feat: Import Deck component

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import Deck from './Deck'
+
 export class DeckList extends Component {
   render() {
     return (
       <View>
         <Text> Deck List </Text>
+        <Deck />
       </View>
     )
   }
