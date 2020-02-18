1d63bf9cb722656214dc7a3525c302e984639857
MFAC <ninafroes@gmail.com>
2020-02-18 23:43:40 +0100

    feat: Import NewDeck

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <DeckList />
+      <NewDeck />
     </View>
   )
 }
