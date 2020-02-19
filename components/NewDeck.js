47c3e29a36ffed826d4ac9d6ef64509f787ef961
MFAC <ninafroes@gmail.com>
2020-02-19 21:44:42 +0100

    styles: Adjust styles

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View, TextInput, StyleSheet } from 'react-native'
-import { white } from '../utils/color'
+import { white, gainsboro } from '../utils/color'
 
 import Btn from './Btn'
 
@@ -16,12 +16,12 @@ export class NewDeck extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> New Deck </Text>
         <TextInput
-          style={{ height: 40 }}
-          placeholder="Deck title"
+          style={styles.textInput}
+          placeholder="My Deck"
           onChangeText={(text) => this.setState({ text })}
           value={this.state.text}
+          multiline={true}
         />
         <Btn
           onPress={this.onPress}
@@ -44,5 +44,13 @@ const styles = StyleSheet.create({
     backgroundColor: white,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    backgroundColor: gainsboro,
+    width: '100%',
+    height: 40,
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
   }
 })
