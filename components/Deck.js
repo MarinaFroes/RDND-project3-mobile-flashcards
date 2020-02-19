22f75f2e9e59caa884f0b6d76b10295cd0e50e59
MFAC <ninafroes@gmail.com>
2020-02-19 15:27:51 +0100

    feat: Receiving props from DeckList

--- components/Deck.js
+++ components/Deck.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
-const Deck = () => {
+import { skyblue } from '../utils/color'
+
+const Deck = ({ deckTitle }) => {
   return (
-    <View>
-      <Text>Deck</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{deckTitle}</Text>
     </View>
   )
 }
 
 export default Deck
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: skyblue,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '85%',
+  },
+  text: {
+    fontSize: 22,
+  }
+})
