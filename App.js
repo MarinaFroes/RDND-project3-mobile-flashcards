6c9782150bb276eb2af5534e612a88bcf80a821d
MFAC <ninafroes@gmail.com>
2020-02-19 08:52:51 +0100

    feat: Add NavigationContainer, Stack.Navigator and Stack.Screen

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import DeckList from './components/DeckList'
 import NewDeck from './components/NewDeck'
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <DeckList />
-      <NewDeck />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={DeckList}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="NewDeck" component={NewDeck} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// })
