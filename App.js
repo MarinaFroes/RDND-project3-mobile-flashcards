32908e1ab14ca3d58b259464214a7cf002b34b9e
MFAC <ninafroes@gmail.com>
2020-02-20 12:53:39 +0100

    feat: Remove Home componen

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
+import 'react-native-gesture-handler'
+
 import React from 'react'
-import { View } from 'react-native'
+// import { View } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import Card from './components/Card'
+import NewCard from './components/NewCard'
+import Deck from './components/Deck'
+
+const Stack = createStackNavigator()
 
 import reducer from './reducers/index'
 import middleware from './middleware/index'
-import Home from './components/Home'
+// import Home from './components/Home'
 
 const store = createStore(reducer, middleware)
 
 function App() {
   return (
     <Provider store={store}>
-      <View style={{ flex: 1 }}>
-        <Home />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Decks">
+          <Stack.Screen
+            name='Decks'
+            component={DeckList}
+            options={{ title: 'Home' }}
+          />
+          <Stack.Screen
+            name='New Deck'
+            component={NewDeck}
+          />
+          <Stack.Screen
+            name='Card'
+            component={Card}
+          />
+          <Stack.Screen
+            name='New Card'
+            component={NewCard}
+          />
+          <Stack.Screen
+            name='Deck'
+            component={Deck}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   )
 }
