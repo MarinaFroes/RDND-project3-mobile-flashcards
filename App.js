d8d5b4357022b771c47e49ecbf07960771b1a271
MFAC <ninafroes@gmail.com>
2020-02-20 10:50:23 +0100

    feat: Create store passing middleware

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { View } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers/index'
 
+import reducer from './reducers/index'
+import middleware from './middleware/index'
 import Home from './components/Home'
+
+const store = createStore(reducer, middleware)
 
 function App() {
   return (
-    <Provider store={createStore(reducer)}>
+    <Provider store={store}>
       <View style={{ flex: 1 }}>
         <Home />
       </View>
