8a2449a34ad4d80255d37e4307018783f0d48561
MFAC <ninafroes@gmail.com>
2020-02-20 10:00:46 +0100

    feat: Import FloatBtn component

--- components/DeckList.js
+++ components/DeckList.js
@@ -4,23 +4,16 @@ import { connect } from 'react-redux'
 import { white, mediumvioletred } from '../utils/color'
 
 import Deck from './Deck'
-import Btn from './Btn'
+// import Btn from './Btn'
+import FloatBtn from './FloatBtn'
 
 export class DeckList extends Component {
   render() {
     console.log(this.props.decks)
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          {/* <FlatList
-            data={[
-              { title: 'React' },
-              { title: 'JavaScript' },
-              { title: 'Redux' },
-              { title: 'React Native' }
-            ]}
-            renderItem={({ item }) => <Deck  deckTitle={item.title}/>}
-          /> */}
+      <View>
+        <ScrollView>
+          <Text style={styles.header}>Here is you decks list:</Text>
           <Deck deckTitle="React"/>
           <Deck deckTitle="React"/>
           <Deck deckTitle="React"/>
@@ -29,18 +22,14 @@ export class DeckList extends Component {
           <Deck deckTitle="React"/>
           <Deck deckTitle="React"/>
           <Deck deckTitle="React"/>
-          <Btn
-            onPress={() => this.props.navigation.navigate('New Deck')}
-          >
-            <Text>Create deck</Text>
-          </Btn>
-          <Btn
-            onPress={() => this.props.navigation.navigate('New Card')}
-          >
-            <Text>Create card</Text>
-          </Btn>
+          <Deck deckTitle="React"/>
+          <Deck deckTitle="React"/>
+          <Deck deckTitle="React"/>
+        </ScrollView>
+        <View>
+          <FloatBtn onPress={() => this.props.navigation.navigate('New Deck')}/>
         </View>
-      </ScrollView>
+     </View>
     )
   }
 }
@@ -54,7 +43,21 @@ function mapStateToProps({ decks }) {
 export default connect(mapStateToProps)(DeckList)
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
   },
 })
+
+{/* <Btn
+            onPress={() => this.props.navigation.navigate('New Deck')}
+          >
+            <Text>Create deck</Text>
+          </Btn>
+          <Btn
+            onPress={() => this.props.navigation.navigate('New Card')}
+          >
+            <Text>Create card</Text>
+          </Btn> */}
