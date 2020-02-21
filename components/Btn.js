f8f1b334b498248ab3959e196c3c3be92f638b21
MFAC <ninafroes@gmail.com>
2020-02-21 18:10:18 +0100

    refactor: Remove unused View import

--- components/Btn.js
+++ components/Btn.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { steelblue, white } from '../utils/color'
 
 const Btn = ({ children, onPress}) => {
